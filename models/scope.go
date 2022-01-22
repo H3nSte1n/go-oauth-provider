77dfea6ec176e0c011ca42ace16b4a89b2649316
Henry Steinhauer <henry.steinhauer@i22.de>
2022-01-22 17:17:49 +0100

    feat: Add Service to scope

--- models/scope.go
+++ models/scope.go
@@ -12,6 +12,7 @@ type Scope struct {
 	Write 				*bool 								`json:"write" binding:"required"`
 	Create 				*bool 								`json:"create" binding:"required"`
 	Delete 				*bool 								`json:"delete" binding:"required"`
+	ServiceID     *primitive.ObjectID   `json:"service_id" binding:"required"`
 	CreatedAt 		*time.Time 						`json:"created_at"`
 	UpdatedAt 		*time.Time 						`json:"updated_at"`
 }
